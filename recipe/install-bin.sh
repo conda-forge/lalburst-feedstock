b6bfd8e1d8d1b856b59aa4c4485b1b5c5e2d0d0b
duncan.macleod <duncan.macleod@ligo.org>
2022-08-12 10:40:12 +0100

    use alias for make
    to make build scripts more readable

--- recipe/install-bin.sh
+++ recipe/install-bin.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
+_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+
 # install from python build directory
 _pybuilddir="_build${PY_VER}"
 cd ${_pybuilddir}
 
 # install binaries
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
+${_make} install -C bin
 
 # install system configuration files
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
+${_make} install-sysconfDATA
